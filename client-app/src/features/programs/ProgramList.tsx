fd6d4443b27c6f55d1b5f77986572f2e92208218
JasonTrapp12 <jwt9552@g.rit.edu>
2025-12-21 22:41:44 -0500

    programs datagrid styling

--- client-app/src/features/programs/ProgramList.tsx
+++ client-app/src/features/programs/ProgramList.tsx
@@ -2,6 +2,7 @@ import { Box, Typography, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import type { IProgram } from "../../models/Program";
 import { programsColumns } from "./ProgramsColumns";
+import { alpha } from "@mui/material/styles";
 
 interface IProgramListProps {
   programs: IProgram[];
@@ -22,6 +23,21 @@ const ProgramList = (props: IProgramListProps) => {
         columns={programsColumns}
         rows={programs.map((program) => ({ ...program, id: program.teamName }))}
         autoHeight
+        hideFooter
+        sx={{
+          borderRadius: 4,
+          "& .MuiDataGrid-columnHeaders, & .MuiDataGrid-columnHeader": {
+            backgroundColor: alpha(theme.palette.secondary.main, 0.22),
+          },
+
+          "& .MuiDataGrid-columnHeader": {
+            color: theme.palette.text.primary,
+            fontWeight: 700,
+          },
+          "& .MuiDataGrid-cell": {
+            color: theme.palette.text.primary,
+          },
+        }}
       />
     </Box>
   );
